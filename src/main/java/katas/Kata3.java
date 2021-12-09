db0a62cf409af38995b96118e8f1f2e27592e52b
David Zuluaga <zuluroa@gmail.com>
2021-12-09 12:35:34 -0500

    Se realiza el ejercicio del Kata 3

--- src/main/java/katas/Kata3.java
+++ src/main/java/katas/Kata3.java
@@ -6,6 +6,7 @@ import model.MovieList;
 import util.DataUtil;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
     Goal: Use map() and flatMap() to project and flatten the movieLists into an array of video ids (flatMap(c -> c.stream()))
@@ -16,6 +17,6 @@ public class Kata3 {
     public static List<Integer> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(1, 2, 3);
+        return movieLists.stream().flatMap(movie -> movie.getVideos().stream()).map(video -> video.getId()).collect(Collectors.toList());
     }
 }
